44fb11a52229ab224ad39651bf8db32e8bfabb29
Duckycolors <codeandwork23@protonmail.com>
2026-02-08 20:06:46 +1100

    center and add divs

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ container = document.querySelector("#container");
 document.getElementById("btn").onclick = function() {
   for (i = 0; i < (divNumberX.value * divNumberY.value); i++) {
   const div1 = document.createElement("div");
+  div1.id = "div1";
   container.appendChild(div1);
   }
 
