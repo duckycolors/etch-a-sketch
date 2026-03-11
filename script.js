2a1599aaf092aaf79b8a9bcb95b0ffb913132683
Duckycolors <codeandwork23@protonmail.com>
2026-03-11 13:32:25 +1100

    first steps of re-do

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let divNumberX = document.getElementById("inputX");
-let divNumberY = document.getElementById("inputY");
+GRIDLENGTH = 600;
 
-divNumberX.value = 16;
-divNumberY.value = 16;
-
+let rows = document.getElementById("inputX");
+let cols = document.getElementById("inputY");
 container = document.querySelector("#container");
 
+container.style.width = `${GRIDLENGTH}px`
+container.style.height = `${GRIDLENGTH}px`
+
+rows.value = 16;
+cols.value = 16;
+
 document.getElementById("btn").onclick = function() {
-  for (i = 0; i < (divNumberX.value * divNumberY.value); i++) {
-  const div1 = document.createElement("div");
-  div1.id = "div1";
-  container.appendChild(div1);
+  for (i = 0; i < (rows.value * cols.value); i++) {
+  const gridCell = document.createElement("div");
+  gridCell.id = "div1";
+  container.appendChild(gridCell);
   }
 
 }
