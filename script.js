81aabaec75c750fca27fc69042ee2ea8b2089c29
Duckycolors <codeandwork23@protonmail.com>
2026-03-11 13:52:55 +1100

    grid calculations now working

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ let rows = document.getElementById("inputX");
 let cols = document.getElementById("inputY");
 container = document.querySelector("#container");
 
-container.style.width = `${GRIDLENGTH}px`
-container.style.height = `${GRIDLENGTH}px`
-
 rows.value = 16;
 cols.value = 16;
 
+container.style.width = `${GRIDLENGTH}px`
+container.style.height = `${GRIDLENGTH}px`
+
 document.getElementById("btn").onclick = function() {
   for (i = 0; i < (rows.value * cols.value); i++) {
-  const gridCell = document.createElement("div");
-  gridCell.id = "div1";
-  container.appendChild(gridCell);
+    const gridCell = document.createElement("div");
+    
+    gridCell.style.width = `${GRIDLENGTH / cols.value - 2}px`;
+    gridCell.style.height = `${GRIDLENGTH / rows.value - 2}px`;
+    gridCell.classList.add("cell");
+
+    container.appendChild(gridCell);
   }
 
 }
