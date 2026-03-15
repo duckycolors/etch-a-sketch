ad612bae44678953499a5e139529fe3dddde11dd
Duckycolors <codeandwork23@protonmail.com>
2026-03-15 19:00:52 +1100

    colour function added

--- script.js
+++ script.js
@@ -2,13 +2,54 @@ GRIDLENGTH = 600;
 
 let gridInput = document.getElementById("gridNumber");
 let gridInputNumber = document.getElementById("gridNumberLabel");
-container = document.querySelector("#container");
+let container = document.querySelector("#container");
+let colourSelector = document.querySelector("#colourselect");
 
 gridInput.value = 16;
 gridInputNumber.textContent = `Grid Number: ${gridInput.value}`;
 
 gridInput.addEventListener("input", function() {
   gridInputNumber.textContent = `Grid Number: ${gridInput.value}`;
+  if(gridInput.value < 10) {
+    gridInputNumber.style.color = "red";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 20) {
+    gridInputNumber.style.color = "orange";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 30) {
+    gridInputNumber.style.color = "goldenrod";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 40) {
+    gridInputNumber.style.color = "limegreen";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 50) {
+    gridInputNumber.style.color = "lime";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 60) {
+    gridInputNumber.style.color = "lightblue";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 70) {
+    gridInputNumber.style.color = "dodgerblue";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 80) {
+    gridInputNumber.style.color = "mediumorchid";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 90) {
+    gridInputNumber.style.color = "pink";
+    gridInputNumber.style.transition = "0.5s";
+  }
+  if(gridInput.value >= 99) {
+    gridInputNumber.style.color = "deeppink";
+    gridInputNumber.style.transition = "0.5s";
+  }
 });
 
 container.style.width = `${GRIDLENGTH}px`
@@ -25,16 +66,13 @@ document.getElementById("btn").onclick = function() {
     container.appendChild(gridCell);
 
     gridCell.addEventListener("mouseover", function(e) {
-        gridCell.style.backgroundColor = "lightblue";
+        gridCell.style.backgroundColor = colourSelector.value;
     })
     btn.addEventListener("click", function() {
         gridCell.remove("gridCell");
     });
   } 
 }
-// function changeGridColour() {
-//   this.style.backgroundColor = "red";
-// }
 
 
 
